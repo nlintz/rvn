aeb96cd294697f61d5d215143ce33b72062ab416
David Byrd <david@byrdhou.se>
2012-11-29 03:37:29 -0500

    added client side product load

--- public/js/app.js
+++ public/js/app.js
@@ -1,116 +1,228 @@
 var products = [
   {
-    name :'Phoenix Embroidery Jacquard Dress'
+    name :'Phoenix Embroidery Jacquard Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/00.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/00.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/00.jpg'
   },
   {
-    name :'Cutout Illusion Jacquard Pants'
+    name :'Cutout Illusion Jacquard Pants',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/01.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/01.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/01.jpg'
   },
   {
-    name :'Cutout Illusion Jacquard Fishtail Dress'
+    name :'Cutout Illusion Jacquard Fishtail Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/02.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/02.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/02.jpg'
   },
   {
-    name :'Aztec Jacquard Mini Dress'
+    name :'Aztec Jacquard Mini Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/03.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/03.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/03.jpg'
   },
   {
-    name :'Tattoo Lace Jacquard Flared Dress'
+    name :'Tattoo Lace Jacquard Flared Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/04.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/04.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/04.jpg'
   },
   {
-    name :'Phoenix Embroidery High Waist Jacquard Skirt'
+    name :'Phoenix Embroidery High Waist Jacquard Skirt',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/05.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/05.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/05.jpg'
   },
   {
-    name :'Phoenix Embroidery High Waist Jacquard Skirt'
+    name :'Phoenix Embroidery High Waist Jacquard Skirt',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/06.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/06.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/06.jpg'
   },
   {
-    name :'Phoenix Embroidery Jacquard Dress'
+    name :'Phoenix Embroidery Jacquard Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/07.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/07.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/07.jpg'
   },
   {
-    name :'Ocelot Jacquard Tank Flared Dress'
+    name :'Ocelot Jacquard Tank Flared Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/08.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/08.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/08.jpg'
   },
   {
-    name :'Cutout Illusion Frenchy Pullover'
+    name :'Cutout Illusion Frenchy Pullover',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/09.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/09.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/09.jpg'
   },
   {
-    name :'Tattoo Lace Jacquard Flared Dress'
+    name :'Tattoo Lace Jacquard Flared Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/10.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/10.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/10.jpg'
   },
   {
-    name :'Cutout Illusion Jacqurad Maxi Dress'
+    name :'Cutout Illusion Jacqurad Maxi Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/11.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/11.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/11.jpg'
   },
   {
-    name :'Cutout Illusion Jacqurad Pants'
+    name :'Cutout Illusion Jacqurad Pants',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/12.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/12.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/12.jpg'
   },
   {
-    name :'Aztec Jacquard Mini Dress'
+    name :'Aztec Jacquard Mini Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/13.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/13.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/13.jpg'
   },
   {
-    name :'Ocelot Jacquard Tank Flared Dress'
+    name :'Ocelot Jacquard Tank Flared Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/14.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/14.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/14.jpg'
   },
   {
-    name :'Cutout Illusion Frenchy Pullover'
+    name :'Cutout Illusion Frenchy Pullover',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/15.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/15.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/15.jpg'
   },
   {
-    name :'Cutout Illusion Jacqurad Maxi Dress'
+    name :'Cutout Illusion Jacqurad Maxi Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/16.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/17.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/18.jpg'
   },
   {
-    name :'Tattoo Lace Jacquard Flared Dress'
+    name :'Tattoo Lace Jacquard Flared Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/19.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/19.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/19.jpg'
   },
   {
-    name :'Cutout Illusion Jacquard Long Sleeve Dress'
+    name :'Cutout Illusion Jacquard Long Sleeve Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/20.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/20.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/20.jpg'
   },
   {
-    name :'Ocelot Jacquard Tank Flared Dress'
+    name :'Ocelot Jacquard Tank Flared Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/21.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/21.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/21.jpg'
   },
   {
-    name :'Cutout Illusion Jacquard Pants'
+    name :'Cutout Illusion Jacquard Pants',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/22.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/22.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/22.jpg'
   },
   {
-    name :'Aztec Jacquard Mini Dress'
+    name :'Aztec Jacquard Mini Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/23.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/23.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/23.jpg'
   },
   {
-    name :'Aztec Jacquard Mini Skirt'
+    name :'Aztec Jacquard Mini Skirt',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/24.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/24.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/24.jpg'
   },
   {
-    name :'Cutout Illusion Jacquard Pants'
+    name :'Cutout Illusion Jacquard Pants',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/25.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/25.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/25.jpg'
   },
   {
-    name :'Cutout Illusion Frenchy Pullover'
+    name :'Cutout Illusion Frenchy Pullover',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/26.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/26.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/26.jpg'
   },
   {
-    name :'Tattoo Lace Jacquard Flared Dress'
+    name :'Tattoo Lace Jacquard Flared Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/27.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/27.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/27.jpg'
   },
   {
-    name :'Cutout Illusion Jacquard Pants'
+    name :'Cutout Illusion Jacquard Pants',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/28.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/28.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/28.jpg'
   },
   {
-    name :'Cutout Illusion Frenchy Pullover'
+    name :'Cutout Illusion Frenchy Pullover',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/29.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/29.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/29.jpg'
   },
   {
-    name :'Cutout Illusion Jacqurad Maxi Dress'
+    name :'Cutout Illusion Jacqurad Maxi Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/30.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/30.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/30.jpg'
   },
   {
-    name :'Tattoo Lace Jacquard Flared Dress'
+    name :'Tattoo Lace Jacquard Flared Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/31.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/31.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/31.jpg'
   },
   {
-    name :'Aztec Jacquard Mini Skirt'
+    name :'Aztec Jacquard Mini Skirt',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/32.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/32.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/32.jpg'
   },
   {
-    name :'Aztec Jacquard Mini Dress'
+    name :'Aztec Jacquard Mini Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/33.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/33.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/33.jpg'
   },
   {
-    name :'Aztec Jacquard Mini Dress'
+    name :'Aztec Jacquard Mini Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/34.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/34.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/34.jpg'
   },
   {
-    name :'Aztec Jacquard Mini Skirt'
+    name :'Aztec Jacquard Mini Skirt',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/35.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/35.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/35.jpg'
   },
   {
-    name :'Ocelot Jacquard Tank Flared Dress'
+    name :'Ocelot Jacquard Tank Flared Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/00.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/00.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/00.jpg'
   },
   {
-    name :'U &ndash; Neck Geo Signature Dress'
+    name :'U &ndash; Neck Geo Signature Dress',
+    small: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/small/36.jpg',
+    medium: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/medium/36.jpg',
+    large: 'http://d3phq4e9hcpjua.cloudfront.net/rvn/large/36.jpg'
   }
 ];
 
 $(document).ready(function () {
-
+  var thumb = function (small) { return "<li><img src='" + small + "'/></li>"; };
+  for (var product in products) {
+    $('.dresses ul').append(thumb(products[product].small));
+  }
+  var current_product = 15;
   $('.r_arrow, .right').click(function (event) {
 
   });
