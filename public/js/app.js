787b11f9acd86425afd40ed05bdb4c3c12c7b08f
David Byrd <david@byrdhou.se>
2012-11-29 02:54:01 -0500

    save

---
+++ public/js/app.js
@@ -0,0 +1,124 @@
+var products = [
+  {
+    name :'Phoenix Embroidery Jacquard Dress'
+  },
+  {
+    name :'Cutout Illusion Jacquard Pants'
+  },
+  {
+    name :'Cutout Illusion Jacquard Fishtail Dress'
+  },
+  {
+    name :'Aztec Jacquard Mini Dress'
+  },
+  {
+    name :'Tattoo Lace Jacquard Flared Dress'
+  },
+  {
+    name :'Phoenix Embroidery High Waist Jacquard Skirt'
+  },
+  {
+    name :'Phoenix Embroidery High Waist Jacquard Skirt'
+  },
+  {
+    name :'Phoenix Embroidery Jacquard Dress'
+  },
+  {
+    name :'Ocelot Jacquard Tank Flared Dress'
+  },
+  {
+    name :'Cutout Illusion Frenchy Pullover'
+  },
+  {
+    name :'Tattoo Lace Jacquard Flared Dress'
+  },
+  {
+    name :'Cutout Illusion Jacqurad Maxi Dress'
+  },
+  {
+    name :'Cutout Illusion Jacqurad Pants'
+  },
+  {
+    name :'Aztec Jacquard Mini Dress'
+  },
+  {
+    name :'Ocelot Jacquard Tank Flared Dress'
+  },
+  {
+    name :'Cutout Illusion Frenchy Pullover'
+  },
+  {
+    name :'Cutout Illusion Jacqurad Maxi Dress'
+  },
+  {
+    name :'Tattoo Lace Jacquard Flared Dress'
+  },
+  {
+    name :'Cutout Illusion Jacquard Long Sleeve Dress'
+  },
+  {
+    name :'Ocelot Jacquard Tank Flared Dress'
+  },
+  {
+    name :'Cutout Illusion Jacquard Pants'
+  },
+  {
+    name :'Aztec Jacquard Mini Dress'
+  },
+  {
+    name :'Aztec Jacquard Mini Skirt'
+  },
+  {
+    name :'Cutout Illusion Jacquard Pants'
+  },
+  {
+    name :'Cutout Illusion Frenchy Pullover'
+  },
+  {
+    name :'Tattoo Lace Jacquard Flared Dress'
+  },
+  {
+    name :'Cutout Illusion Jacquard Pants'
+  },
+  {
+    name :'Cutout Illusion Frenchy Pullover'
+  },
+  {
+    name :'Cutout Illusion Jacqurad Maxi Dress'
+  },
+  {
+    name :'Tattoo Lace Jacquard Flared Dress'
+  },
+  {
+    name :'Aztec Jacquard Mini Skirt'
+  },
+  {
+    name :'Aztec Jacquard Mini Dress'
+  },
+  {
+    name :'Aztec Jacquard Mini Dress'
+  },
+  {
+    name :'Aztec Jacquard Mini Skirt'
+  },
+  {
+    name :'Ocelot Jacquard Tank Flared Dress'
+  },
+  {
+    name :'U &ndash; Neck Geo Signature Dress'
+  }
+];
+
+$(document).ready(function () {
+
+  $('.r_arrow, .right').click(function (event) {
+
+  });
+
+  $('.l_arrow .left').click(function (event) {
+    var old_middle = $('.center img').attr('src');
+    var old_left = $('.left img.side').attr('src');
+    var old_right = $('.right img.side').attr('src');
+  });
+
+});
